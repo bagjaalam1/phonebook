2fe38ab88c1fc5e410c8f1a2c673e91bb9b4e589
bagjaalam1 <bagjaalam1@gmail.com>
2023-06-20 11:19:00 +0700

    menghapus folder yg tidak sengaja dipush

--- hook-redux-toolkit/src/features/add/AddSlice.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { addContactAPI } from "./addAPI";
-
-export const addContact = createAsyncThunk(
-  "phonebook/addContact",
-  addContactAPI
-); // asyncThunk ini untuk slice ContactList
