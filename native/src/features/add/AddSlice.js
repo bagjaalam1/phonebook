7f2b387e3c57eb8a2dd8d0bc5ac5f48d5d0a1109
bagjaalam1 <bagjaalam1@gmail.com>
2023-06-27 18:15:54 +0700

    hapus folder yg salah

--- native/src/features/add/AddSlice.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { addContactAPI } from "./AddAPI";
-
-export const addContact = createAsyncThunk(
-  "phonebook/addContact",
-  addContactAPI
-); // asyncThunk ini untuk slice ContactList
